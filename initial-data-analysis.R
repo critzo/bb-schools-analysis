657f802ebb22d53ba96589b9d69037156b3739ee
Chris Ritzo <critzo@opentechinstitute.org>
2016-11-12 18:41:28 -0500

    Updated script documenting initial numerical and graphical summaries

--- initial-data-analysis.R
+++ initial-data-analysis.R
@@ -23,7 +23,37 @@ summary(PRR_initial_summary)
 
 ## Graphical Summaries
 
-### 
-plot(DownloadThroughput$hour,DownloadThroughput$downloadThroughput, main="All Download Speed Measurements by Hour of the Day", xlab="Hour of the Day", ylab="Download Speed (Mbps)")
+### Histogram of All Download Speed Measurements
+hist(DownloadThroughput$downloadThroughput, main=paste('Histogram of All Download Speed Measurements'), xlab=paste('Download Throughput, Mbps'))
+
+### Kernel Density of All Download Speed Measurements
+plot(density(DownloadThroughput$downloadThroughput),main=paste('Kernel Density of All Download Speed Measurements'))
+
+### Indexed, Sorted Values of All Download Speed Measurements
+plot(sort(DownloadThroughput$downloadThroughput),pch=".",main=paste('Indexed, Sorted Values of All Download Speed Measurements'), ylab="Download Throughput", xlab="All Download Tests, Sorted by Download Speed")
+
+### Distribution of All Download Speed Measurements by Hour of the Day
 require(ggplot2)
 print(qplot(DownloadThroughput$hour,DownloadThroughput$downloadThroughput,main="Distribution of All Download Speed Meaurements by Hour of the Day",xlab="Hour of the Day",ylab="Download Speed (Mbps)"))
+
+### Comparison of School Type - Download Speeds by Hour of the Day
+ggplot(DownloadThroughput,aes( x=hour,y=downloadThroughput))+geom_smooth(se=F)+geom_point()+facet_grid(grade_level~.)+ggtitle("Download Speed Measurements by Grade Level, by Hour")+labs(x="Hour of the Day",y="Download Throughput (Mbps)")
+
+### Comparison of Schools - Download Speeds by Hour of the Day
+ggplot(DownloadThroughput,aes( x=hour,y=downloadThroughput))+geom_point()+facet_grid(~school)+ggtitle("Download Measurements by Hour, Comparing by School")+labs(y="Download Throughput (Mbps)", x="Hour of the Day")
+
+### Comparison of Classrooms and Help Desk - Download Speeds by Hour of the Day
+ggplot(DownloadThroughput,aes( x=hour,y=downloadThroughput))+geom_smooth(se=F)+geom_point()+facet_grid(room~.)+ggtitle("Download Speed Measurements by Classroom, by Hour")+labs(x="Hour of the Day",y="Download Throughput (Mbps)")
+
+### Comparison of Elementary and High School Classrooms - Download Speeds by Hour
+ggplot(filter(DownloadThroughput, room != 'help desk'),aes( x=hour,y=downloadThroughput))+geom_smooth(se=F)+geom_point()+facet_grid(room~.)+ggtitle("Download Speed Measurements by Classroom, by Hour")+labs(x="Hour of the Day",y="Download Throughput (Mbps)")
+
+### Elementary and High School Classrooms only, by Weekday
+ggplot(filter(DownloadThroughput, room != 'help desk'), aes(y=downloadThroughput, x=weekday)) + geom_bar(stat="identity")+facet_wrap(~ grade_level)
+
+### Comparison of Elementary and High School Download Speed Measurements by Day of the Week
+ggplot(filter(DownloadThroughput, room != 'help desk'),aes( x=weekday,y=downloadThroughput))+geom_point()+facet_grid(grade_level~.)+ggtitle("Download Speed Measurements by Classroom, by Hour")+labs(x="Day of the Week",y="Download Throughput (Mbps)")
+
+### Download Speed Measurements, High School IT Help Desk, by Hour
+ggplot(filter(DownloadThroughput, room == 'help desk'),aes( x=hour,y=downloadThroughput))+geom_smooth(se=F)+geom_point()+facet_grid(room~.)+ggtitle("Download Speed Measurements, High School IT Help Desk, by Hour")+labs(x="Hour of the Day",y="Download Throughput (Mbps)")
+
