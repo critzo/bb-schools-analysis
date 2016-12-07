41df7f6f7e8961fde35c115ff77671cd4509217c
Chris Ritzo <critzo@opentechinstitute.org>
2016-12-07 13:16:28 -0500

    updated numerical summaries by metric, classroom/non-classroom, elem/highschool using newly imported and cleaned data

--- initial-data-analysis.R
+++ initial-data-analysis.R
@@ -3,16 +3,192 @@ require(ggplot2)
 library(dplyr)
 
 ## Load cleaned data 
-raw_and_calc_metrics_withmeta_cleaned = read.csv("<your system path to>/bb-schools-analysis/raw-data/ACPS-raw-data_metrics-calculated_with-metadata_cleaned.csv")
+all_cleaned_data = read.csv("<your system path to>/bb-schools-analysis/raw-data/ACPS-raw-data_metrics-calculated_with-metadata_cleaned.csv")
+
+## Create separate data frames for classroom and non-classroom measurements, elementary and high school measurements 
+classroom_measurements <- subset(filter(all_cleaned_data,room!='help desk'))
+non_classroom_measurements <- subset(filter(all_cleaned_data,room=='help desk'))
+elem_classroom_measurements <- subset(filter(all_cleaned_data,room!='help desk' & grade_level=='elementary'))
+highschool_classroom_measurements <- subset(filter(all_cleaned_data,room!='help desk' & grade_level=='high school'))
 
 ## Numerical Summaries
 
-### Fig. 1 - Calculate the 25th and 75th Percentile Values for Each Metric
-quantile(raw_and_calc_metrics_withmeta_cleaned$downloadThroughput, c(.25,.75),na.rm=TRUE)
-quantile(raw_and_calc_metrics_withmeta_cleaned$uploadThroughput, c(.25,.75),na.rm=TRUE)
+### Fig. 1 - Total number of measurements, 25th and 75th Percentile Values
 
-quantile(RoundTripTime$min_rtt, c(.25,.75))
-quantile(PacketRetransmissionRate$packet_retransmission_rate, c(.25,.75))
+#### Download measurements       
+length(which(!is.na(all_cleaned_data$downloadThroughput)))
+quantile(all_cleaned_data$downloadThroughput, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(classroom_measurements$downloadThroughput)))
+quantile(classroom_measurements$downloadThroughput, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(elem_classroom_measurements$downloadThroughput)))
+quantile(elem_classroom_measurements$downloadThroughput, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(highschool_classroom_measurements$downloadThroughput)))
+quantile(highschool_classroom_measurements$downloadThroughput, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(non_classroom_measurements$downloadThroughput)))
+quantile(non_classroom_measurements$downloadThroughput, c(.25,.75),na.rm=TRUE)
+
+# All     25th      75th      | Classrooms  25th      75th      | Non-Classrooms  25th      75th  
+# 7608    79.59180  91.66299  | 6663        77.36369  90.85208  | 945             185.4724  286.9072
+#                             |---------------------------------|
+#                             | Elementary                      |
+#                             | 4717        78.70964  91.02981  |
+#                             |                                 |
+#                             | High School                     |
+#                             | 1946        74.82725  89.55848  |
+
+#### Upload measurements
+length(which(!is.na(all_cleaned_data$uploadThroughput)))
+quantile(all_cleaned_data$uploadThroughput, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(classroom_measurements$uploadThroughput)))
+quantile(classroom_measurements$uploadThroughput, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(elem_classroom_measurements$uploadThroughput)))
+quantile(elem_classroom_measurements$uploadThroughput, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(highschool_classroom_measurements$uploadThroughput)))
+quantile(highschool_classroom_measurements$uploadThroughput, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(non_classroom_measurements$uploadThroughput)))
+quantile(non_classroom_measurements$uploadThroughput, c(.25,.75),na.rm=TRUE)
+
+# All     25th      75th      | Classrooms  25th      75th      | Non-Classrooms  25th      75th  
+# 9088    63.03321  68.05603  | 7947        62.98748  63.48879  | 1141            219.6216  293.9745
+#                             |---------------------------------|
+#                             | Elementary                      |
+#                             | 5686        62.92426  63.38304  |
+#                             |                                 |
+#                             | High School                     |
+#                             | 2261        63.33436  68.51997  |
+
+#### Average RTT measurements
+length(which(!is.na(all_cleaned_data$avgRTT)))
+quantile(all_cleaned_data$avgRTT, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(classroom_measurements$avgRTT)))
+quantile(classroom_measurements$avgRTT, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(elem_classroom_measurements$avgRTT)))
+quantile(elem_classroom_measurements$avgRTT, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(highschool_classroom_measurements$avgRTT)))
+quantile(highschool_classroom_measurements$avgRTT, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(non_classroom_measurements$avgRTT)))
+quantile(non_classroom_measurements$avgRTT, c(.25,.75),na.rm=TRUE)
+
+# All     25th      75th      | Classrooms  25th      75th      | Non-Classrooms  25th      75th  
+# 7608    4.918611  8.219872  | 6663        5.272273  9.633781  | 945             3.465868  4.348356
+#                             |---------------------------------|
+#                             | Elementary                      |
+#                             | 4717        6.661709 10.220621  |
+#                             |                                 |
+#                             | High School                     |
+#                             | 1946        4.634802 5.829468   |
+
+#### Minimum RTT measurements
+length(which(!is.na(all_cleaned_data$minRTT)))
+quantile(all_cleaned_data$minRTT, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(classroom_measurements$minRTT)))
+quantile(classroom_measurements$minRTT, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(elem_classroom_measurements$minRTT)))
+quantile(elem_classroom_measurements$minRTT, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(highschool_classroom_measurements$minRTT)))
+quantile(highschool_classroom_measurements$minRTT, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(non_classroom_measurements$minRTT)))
+quantile(non_classroom_measurements$minRTT, c(.25,.75),na.rm=TRUE)
+
+# All     25th      75th      | Classrooms  25th      75th      | Non-Classrooms  25th      75th  
+# 16696   3         4         | 14610       3         5         | 2086            3         4
+#                             |---------------------------------|
+#                             | Elementary                      |
+#                             | 10403       3         8         |
+#                             |                                 |
+#                             | High School                     |
+#                             | 4207        3         4         |
+
+#### Packet Retransmission Rate
+length(which(!is.na(all_cleaned_data$packetRetransRate)))
+quantile(all_cleaned_data$packetRetransRate, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(classroom_measurements$packetRetransRate)))
+quantile(classroom_measurements$packetRetransRate, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(elem_classroom_measurements$packetRetransRate)))
+quantile(elem_classroom_measurements$packetRetransRate, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(highschool_classroom_measurements$packetRetransRate)))
+quantile(highschool_classroom_measurements$packetRetransRate, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(non_classroom_measurements$packetRetransRate)))
+quantile(non_classroom_measurements$packetRetransRate, c(.25,.75),na.rm=TRUE)
+
+# All     25th      75th      | Classrooms  25th      75th      | Non-Classrooms  25th      75th  
+# 7608    4.918611  8.219872  | 6663  0.002073605  0.004796193  | 945         0.0008766026  0.0020511029 
+#                             |---------------------------------|
+#                             | Elementary                      |
+#                             | 4717   0.002438402 0.005120240       |
+#                             |                                 |
+#                             | High School                     |
+#                             | 1946   0.001590311 0.002965130     |
+
+#### Network Limited Ratio
+length(which(!is.na(all_cleaned_data$NetworkLimRatio)))
+quantile(all_cleaned_data$NetworkLimRatio, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(classroom_measurements$NetworkLimRatio)))
+quantile(classroom_measurements$NetworkLimRatio, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(elem_classroom_measurements$NetworkLimRatio)))
+quantile(elem_classroom_measurements$NetworkLimRatio, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(highschool_classroom_measurements$NetworkLimRatio)))
+quantile(highschool_classroom_measurements$NetworkLimRatio, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(non_classroom_measurements$NetworkLimRatio)))
+quantile(non_classroom_measurements$NetworkLimRatio, c(.25,.75),na.rm=TRUE)
+
+# All     25th      75th      | Classrooms  25th      75th      | Non-Classrooms  25th      75th  
+# 7608    0.9755999 0.9821918 | 6663        0.9759522 0.9822473 | 945             0.9737537 0.9818442
+#                             |---------------------------------|
+#                             | Elementary                      |
+#                             | 4717       0.9763309 0.9823665   |
+#                             |                                 |
+#                             | High School                     |
+#                             | 1946       0.9748452 0.9819561  |
+
+#### Client Limited Ratio
+length(which(!is.na(all_cleaned_data$ClientLimRatio)))
+quantile(all_cleaned_data$ClientLimRatio, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(classroom_measurements$ClientLimRatio)))
+quantile(classroom_measurements$ClientLimRatio, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(elem_classroom_measurements$ClientLimRatio)))
+quantile(elem_classroom_measurements$ClientLimRatio, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(highschool_classroom_measurements$ClientLimRatio)))
+quantile(highschool_classroom_measurements$ClientLimRatio, c(.25,.75),na.rm=TRUE)
+
+length(which(!is.na(non_classroom_measurements$ClientLimRatio)))
+quantile(non_classroom_measurements$ClientLimRatio, c(.25,.75),na.rm=TRUE)
+
+# All     25th        75th          | Classrooms  25th      75th      | Non-Classrooms  25th      75th  
+# 7608  0.0006143714  0.0012038161  | 6663  0.0006625815  0.0012654174| 945         0.000339913   0.001013628
+#                             |---------------------------------|
+#                             | Elementary                      |
+#                             | 4717    0.0006715249 0.0014013503    |
+#                             |                                 |
+#                             | High School                     |
+#                             | 1946    0.0006375036 0.0011517160 |
 
 ### Fig 2 - Display Initial Summary Data for Each Metric
 DL_initial_summary <- subset(DownloadThroughput, select=c(downloadThroughput,school,room,grade_level))
